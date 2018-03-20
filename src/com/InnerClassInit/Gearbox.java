dfdb51a14d859b36b202b9788b2c99b962e41794
deltonmyalil <deltonguns@gmail.com>
2018-03-20 14:39:24 +0530

    Class Nesting example added

--- src/com/InnerClassInit/Gearbox.java
+++ src/com/InnerClassInit/Gearbox.java
@@ -6,15 +6,50 @@ public class Gearbox {
 	private ArrayList<Gear> gears;
 	private int maxgears;
 	private int gearNumber = 0;
+	private boolean clutchIsIn;
+	private int currentGear = 0;
 
 	public Gearbox(int maxgears) {
 		this.maxgears = maxgears;
 		this.gears = new ArrayList<Gear>();
 		Gear neutral = new Gear(0,0.0);
 		this.gears.add(neutral);
+
+		for(int i=0; i<maxgears; i++) {
+			addGear(i, i*5.3);
+		}
 	}
 
-	public class Gear {  // This class is inside the gearbox class. Makes sense as gears are inside gearbox
+	public void operateClutch(boolean in) {
+		this.clutchIsIn = in;
+	}
+
+	public void addGear(int number, double ratio) {
+		if(number>0&&number<=maxgears) {
+			this.gears.add(new Gear(number,ratio));
+		}
+	}
+
+	public void changeGear(int newGear) {
+		if(newGear>=0&&newGear<gears.size()&&this.clutchIsIn) {
+			this.currentGear = newGear;
+			System.out.println("Gear "+newGear+" selected");
+		} else {
+			System.out.println("VVVrrrrrrroooommmmm...!!!!!!!!");
+			this.currentGear = 0;
+		}
+	}
+
+	public double wheelSpeed(int revs) {
+		if(clutchIsIn) {
+			System.out.println("Vrroom in full clutch");
+			return 0.0;
+		} else {
+			return revs*gears.get(currentGear).getRatio();
+		}
+	}
+
+	private class Gear {  // This class is inside the gearbox class. Makes sense as gears are inside gearbox
 		private int gearNumber;
 		private double ratio;
 
@@ -25,6 +60,10 @@ public class Gearbox {
 
 		public double driveSpeed(int revs) {
 			return revs*this.ratio;
+		}
+
+		public double getRatio() {
+			return ratio;
 		}
 	}
 }
--- src/com/InnerClassInit/Main.java
+++ src/com/InnerClassInit/Main.java
@@ -3,6 +3,25 @@ package com.InnerClassInit;
 public class Main {
 	public static void main(String[] args) {
 		Gearbox mcLaren = new Gearbox(6);  //outer class
-		Gearbox.Gear first = mcLaren.new Gear(1, 12.3);  //Inner class
+//		Gearbox.Gear first = mcLaren.new Gear(1, 12.3);  //Inner class
+//		//You need an outer class object to create an instance of inner class. Shown above
+//		System.out.println(first.driveSpeed(1000));
+		//The above code is when the inner class is public
+
+		//The following when the inner class is private
+//		mcLaren.addGear(1,5.3); //use the add gear method
+//		mcLaren.addGear(2,10.6);
+//		mcLaren.addGear(3,15.9);
+
+		mcLaren.operateClutch(true);
+		mcLaren.changeGear(1);
+		mcLaren.operateClutch(false);
+		System.out.println(mcLaren.wheelSpeed(1000));
+		mcLaren.changeGear(2);
+		System.out.println(mcLaren.wheelSpeed(3000));
+		mcLaren.operateClutch(true);
+		mcLaren.changeGear(3);
+		mcLaren.operateClutch(false);
+		System.out.println(mcLaren.wheelSpeed(6000));
 	}
 }
