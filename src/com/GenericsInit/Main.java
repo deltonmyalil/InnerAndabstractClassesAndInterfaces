2ca4335a012a81e1bd42dee25c835e7b438e194e
deltonmyalil <deltonguns@gmail.com>
2018-03-22 18:43:06 +0530

    Added validation for generic Class experiment. Other types of objects cannot be added to Team

--- src/com/GenericsInit/Main.java
+++ src/com/GenericsInit/Main.java
@@ -6,12 +6,16 @@ public class Main {
 		BaseballPlayer john = new BaseballPlayer("John");
 		SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
-		Team keralaBlasters = new Team("KeralaBlasters");
-		keralaBlasters.addPlayer(sasi);
-		keralaBlasters.addPlayer(john);
+		Team<SoccerPlayer> keralaBlasters = new Team<>("KeralaBlasters");
+//		keralaBlasters.addPlayer(sasi);
+//		keralaBlasters.addPlayer(john);
 		keralaBlasters.addPlayer(beckham);
-
 		System.out.println("Team " + keralaBlasters.getName() + " has " + keralaBlasters.numPlayers() + " players");
 
+		Team<BaseballPlayer> knicks = new Team<>("KochiKnicks");
+		knicks.addPlayer(john);
+
+		Team<RugbyPlayer> rugbears = new Team<>("RugBeaars");
+		rugbears.addPlayer(sasi);
 	}
 }
